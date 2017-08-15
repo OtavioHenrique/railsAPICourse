0a00449a1eba9f759837eb0e98e652e5c32c63d4
Otavio Henrique <otavio.henrique@hotmail.com>
2017-08-15 20:28:34 -0300

    Hateos on kind and self links

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
   
-  belongs_to :kind, optional: true
+  belongs_to :kind do 
+    link(:kind) { kind_url(object.kind.id) }
+  end
   has_many :phones
   has_one :address
-
+  
+  link(:self) { contact_url(object.id) }
+  
   def attributes(*args)
     h = super(*args)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
