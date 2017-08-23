9b1da0a912d83beda95f4f9c136a2dcc0b9c52e9
Otavio Henrique <otavio.henrique@hotmail.com>
2017-08-22 23:00:05 -0300

    Relationship routes

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -41,7 +41,12 @@ class KindsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_kind
-      @kind = Kind.find(params[:id])
+      if params[:contact_id]
+        @kind = Contact.find(params[:contact_id]).kind_id
+        return
+      end
+       
+      @kind = Kind.find(kind_id)
     end
 
     # Only allow a trusted parameter "white list" through.
